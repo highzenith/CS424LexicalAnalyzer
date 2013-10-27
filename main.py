0413e0e4a5736e0f390240b6c4d3a27bb8b84907
highzenith <hz20155@gmail.com>
2013-10-26 22:48:38 -0500

    Token Extraction Complete
    Still need to format and create symbol table dictionary.

--- main.py
+++ main.py
@@ -1,56 +1,85 @@
 filename = "testInput.txt"
 inputFile = open(filename, 'rt')
-# STT = [eol, letter, digit, period, (~, ?, :, _, \)]
+# State Trans. Table = [eol, letter, digit, period, (~, ?, :, _, \)]
 STT = [
-    ['noTokenErr', 1, 2, 0, 'charErr'],
-    ['F1', 1, 1, 'F1', 'charErr'],
-    ['F2', 'F2', 2, 3, 'charErr'],
-    ['floatErr', 'floatErr', 4, 'floatErr', 'charErr'],
-    ['F3', 'F3', 4, 'F3', 'charErr']
+    ['noTokenErr', 1, 2, 'floatErr', 'charErr', 0],
+    ['F1', 1, 1, 'F1', 'charErr', 'F1'],
+    ['F2', 'F2', 2, 3, 'charErr', 'F2'],
+    ['floatErr', 'floatErr', 4, 'floatErr', 'charErr', 'floatErr'],
+    ['F3', 'F3', 4, 'F3', 'charErr', 'F3']
 ]
 
 
 def main():
-    line = inputFile.readline()
-    token = NextToken(line)
-    print(repr(token))
-    # print(str(STT[2][0]))
+    #TODO: Add Header
+    lineNum = 1
+    while True:
+        line = inputFile.readline()
+        if len(line) == 0:
+            break
+        token = NextToken(line)
+        print("\nLine " + str(lineNum) + " Token: " + repr(token))
+        lineNum += 1
 
 
 def NextToken(inputLine):
     state = 0
-    print("State is: " + str(state))
-    while state not in {'F1', 'F2', 'F3'} or state == 0:
-        if len(inputLine) == 0:
-            print("End of File Reached")
-            break
-        for i, ch in enumerate(inputLine):
-            chInt = CharToIntCode(ch)
-            newState = STT[state][chInt]
-            print("State is: " + str(newState))
-            if newState not in {
-                'F1', 'F2', 'F3', 'noTokenErr', 'charErr', 'floatErr'
+    valStart = 0
+    for i, ch in enumerate(inputLine):
+        chInt = CharToIntCode(ch)
+        if chInt == 5:
+            valStart += 1
+        newState = STT[state][chInt]
+        if newState not in {
+            'F1', 'F2', 'F3', 'noTokenErr', 'charErr', 'floatErr'
+        }:
+            state = newState
+        elif newState == 'F1':
+            #token = (type, value)
+            tokenValue = inputLine[(valStart - 1):i].lower()
+            if tokenValue in {
+                'bool', 'char', 'else', 'false', 'float',
+                'if', 'int', 'main', 'true', 'while'
             }:
-                state = newState
-            elif newState == 'F1':
-                #token = (type, value)
-                tokenValue = inputLine[0:i].lower()
-                if tokenValue in {
-                    'bool', 'char', 'else', 'false', 'float',
-                    'if', 'int', 'main', 'true', 'while'
-                }:
-                    tokenType = "keyword"
-                else:
-                    tokenType = "identifier"
-                newToken = tokenType, tokenValue
-                return newToken
+                tokenType = "keyword"
+            else:
+                tokenType = "identifier"
+            newToken = tokenType, tokenValue
+            return newToken
+        elif newState == 'F2':
+            tokenValue = inputLine[(valStart - 1):i]
+            tokenType = "integer"
+            newToken = tokenType, tokenValue
+            return newToken
+        elif newState == 'F3':
+            tokenValue = inputLine[(valStart - 1):i]
+            tokenType = "float"
+            newToken = tokenType, tokenValue
+            return newToken
+        elif newState == 'noTokenErr':
+            tokenValue = "NO TOKEN"
+            tokenType = "ERROR"
+            newToken = tokenType, tokenValue
+            return newToken
+        elif newState == 'charErr':
+            tokenValue = "ILLEGAL CHARACTER: " + repr(ch)
+            tokenType = "ERROR"
+            newToken = tokenType, tokenValue
+            return newToken
+        elif newState == "floatErr":
+            tokenValue = "ILLEGAL FLOAT"
+            tokenType = "ERROR"
+            newToken = tokenType, tokenValue
+            return newToken
+
 
 
 def CharToIntCode(char):
-    print("Char ->" + str(char) + "<-")
     testString = char
     # Test for eol/newline char
-    if testString.isalpha():
+    if testString == "\\n":
+        return 0
+    elif testString.isalpha():
         return 1
     elif testString.isdigit():
         return 2
@@ -59,7 +88,7 @@ def CharToIntCode(char):
     elif testString in {'~', '?', ':', '_', "\\"}:
         return 4
     else:
-        return 0
+        return 5
 
 
 if __name__ == '__main__':
