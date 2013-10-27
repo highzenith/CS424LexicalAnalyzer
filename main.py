e289a0ba6a285a4541f5ba068b0081b3fbcf7668
highzenith <hz20155@gmail.com>
2013-10-26 20:06:17 -0500

    Keywords and Identifiers Implemented

--- main.py
+++ main.py
@@ -10,34 +10,47 @@ STT = [
 ]
 
 
-def NextToken():
+def main():
+    line = inputFile.readline()
+    token = NextToken(line)
+    print(repr(token))
+    # print(str(STT[2][0]))
+
+
+def NextToken(inputLine):
     state = 0
+    print("State is: " + str(state))
     while state not in {'F1', 'F2', 'F3'} or state == 0:
-        inputLine = inputFile.readline()
+        if len(inputLine) == 0:
+            print("End of File Reached")
+            break
         for i, ch in enumerate(inputLine):
-            if ch == '':
-                print("End of File Reached")
-                break
-            if ch in {' ', '(', ')', ';', '{', '}', ',', '/', '+', '=', '*'}:
-                continue
             chInt = CharToIntCode(ch)
             newState = STT[state][chInt]
-            print("State is: " + str(newState) + " chInt is: " + str(chInt))
+            print("State is: " + str(newState))
             if newState not in {
                 'F1', 'F2', 'F3', 'noTokenErr', 'charErr', 'floatErr'
             }:
                 state = newState
-            else:
-                state = newState
+            elif newState == 'F1':
+                #token = (type, value)
+                tokenValue = inputLine[0:i].lower()
+                if tokenValue in {
+                    'bool', 'char', 'else', 'false', 'float',
+                    'if', 'int', 'main', 'true', 'while'
+                }:
+                    tokenType = "keyword"
+                else:
+                    tokenType = "identifier"
+                newToken = tokenType, tokenValue
+                return newToken
 
 
 def CharToIntCode(char):
+    print("Char ->" + str(char) + "<-")
     testString = char
     # Test for eol/newline char
-    if testString == '\n':
-        return 0
-        print("newline")
-    elif testString.isalpha():
+    if testString.isalpha():
         return 1
     elif testString.isdigit():
         return 2
@@ -45,11 +58,8 @@ def CharToIntCode(char):
         return 3
     elif testString in {'~', '?', ':', '_', "\\"}:
         return 4
-
-
-def main():
-    NextToken()
-    #print(str(STT[2][0]))
+    else:
+        return 0
 
 
 if __name__ == '__main__':
